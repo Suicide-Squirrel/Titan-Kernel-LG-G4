f459837a531b11886099ce7d2ba915bb073c042a
Thomas Gleixner <tglx@linutronix.de>
2014-02-23 21:40:23 +0000

    genirq: Move kstat_incr_irqs_this_cpu() to core
    No more users outside the core code. Put it into the poison
    cabinet. That also gets rid of the linux/irq.h include in
    kernel_stat.h
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/20140223212739.124207133@linutronix.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Change-Id: I402391bf7e3e21b54e9069823f715e24b391e7da

--- include/linux/kernel_stat.h
+++ include/linux/kernel_stat.h
@@ -51,14 +51,7 @@ DECLARE_PER_CPU(struct kernel_cpustat, kernel_cpustat);
 
 extern unsigned long long nr_context_switches(void);
 
-#include <linux/irq.h>
 extern unsigned int kstat_irqs_cpu(unsigned int irq, int cpu);
-
-#define kstat_incr_irqs_this_cpu(irqno, DESC)		\
-do {							\
-	__this_cpu_inc(*(DESC)->kstat_irqs);		\
-	__this_cpu_inc(kstat.irqs_sum);			\
-} while (0)
 
 static inline void kstat_incr_softirqs_this_cpu(unsigned int irq)
 {
