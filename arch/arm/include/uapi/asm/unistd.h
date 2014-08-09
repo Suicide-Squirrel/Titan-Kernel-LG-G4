610961610bc460ab7e12b24c79558b7316e21f74
Russell King <rmk+kernel@arm.linux.org.uk>
2014-08-09 08:43:11 +0100

    ARM: wire up memfd_create syscall
    Add the memfd_create syscall to ARM.
    
    Change-Id: I4ffe6563be85cd713c609dcc3c66912aa0a11fa6
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/uapi/asm/unistd.h
+++ arch/arm/include/uapi/asm/unistd.h
@@ -411,6 +411,7 @@
 #define __NR_renameat2			(__NR_SYSCALL_BASE+382)
 #define __NR_seccomp			(__NR_SYSCALL_BASE+383)
 #define __NR_getrandom			(__NR_SYSCALL_BASE+384)
+#define __NR_memfd_create		(__NR_SYSCALL_BASE+385)
 
 /*
  * This may need to be greater than __NR_last_syscall+1 in order to
