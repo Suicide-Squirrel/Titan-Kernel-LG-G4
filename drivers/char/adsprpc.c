2d8ede74b6cb0df8d1c08c50931da1d0357b0403
Mitchel Humpherys <mitchelh@codeaurora.org>
2015-01-27 16:23:14 -0800

    msm: ADSPRPC: Invalidate buffers using physical address
    Cache operations via userspace address has a risk of a page
    fault as on arm64 these addresses are marked as write protect
    and fixed up on first access. To avoid aborts happening when
    the mmap semaphore is locked at this time, invalidate the
    buffers using the physical address.
    
    Change-Id: I4a24b4959df374719c89ba8b6262412ea0eda07d
    Acked-by: Sathish Ambley <sambley@qti.qualcomm.com>
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>
    
    msm: ADSPRPC: Clear output buffers before cache invalidation
    
    Cache operations via userspace address has a risk of a page
    fault as on arm64 these addresses are marked as write protect
    and fixed up on first access. To avoid aborts happening when
    the mmap semaphore is locked at this time, clear non-overlapped
    output buffers before caches could be invalidated.
    
    Change-Id: Iedb42e6d3dca4530b1cf065ea87fee1befea3bc6
    Acked-by: Sathish Ambley <sambley@qti.qualcomm.com>
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>
    
    msm: ADSPRPC: Validate channel status before write
    
    Make sure that the channel is not closed before attempting
    to write to the SMD channel.
    
    Change-Id: I4ff4f57159c163ab4d330dbf4634ebf0b50931a2
    Acked-by: Sathish Ambley <sambley@qti.qualcomm.com>
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -33,6 +33,7 @@
 #include <linux/of_address.h>
 #include <linux/of_platform.h>
 #include <linux/dma-contiguous.h>
+#include <linux/dma-buf.h>
 #include <linux/iommu.h>
 #include <linux/kref.h>
 #include <linux/sort.h>
@@ -773,6 +774,53 @@ static int get_page_list(uint32_t kernel, struct smq_invoke_ctx *ctx)
 	return err;
 }
 
+static inline int is_overlapped_outbuf(struct smq_invoke_ctx *ctx, int oix)
+{
+	int inbufs, outbufs;
+
+	inbufs = REMOTE_SCALARS_INBUFS(ctx->sc);
+	outbufs = REMOTE_SCALARS_OUTBUFS(ctx->sc);
+	if (!ctx->overps[oix]->mstart)
+		return 1;
+	oix = oix + 1;
+	if ((oix < inbufs + outbufs) && !ctx->overps[oix]->mstart &&
+			ctx->overps[oix]->raix < inbufs)
+		return 1;
+	return 0;
+}
+
+static int clear_user_outbufs(struct smq_invoke_ctx *ctx)
+{
+	remote_arg_t *pra = ctx->pra;
+	remote_arg_t *rpra = ctx->rpra;
+	uintptr_t ptr, end;
+	int oix, err = 0;
+	int inbufs = REMOTE_SCALARS_INBUFS(ctx->sc);
+	int outbufs = REMOTE_SCALARS_OUTBUFS(ctx->sc);
+
+	for (oix = 0; oix < inbufs + outbufs; ++oix) {
+		int i = ctx->overps[oix]->raix;
+		if ((i < inbufs) || (pra[i].buf.pv != rpra[i].buf.pv) ||
+			is_overlapped_outbuf(ctx, oix))
+			continue;
+		VERIFY(err, 0 == clear_user(rpra[i].buf.pv,
+				(rpra[i].buf.len < 8) ? rpra[i].buf.len : 8));
+		if (err)
+			goto bail;
+		ptr = buf_page_start(rpra[i].buf.pv) + PAGE_SIZE;
+		end = (uintptr_t)rpra[i].buf.pv + rpra[i].buf.len;
+		for (; ptr < end; ptr += PAGE_SIZE) {
+			VERIFY(err, 0 == clear_user((void *)ptr,
+					((end - ptr) < 8) ? end - ptr : 8));
+			if (err)
+				goto bail;
+		}
+	}
+
+ bail:
+	return err;
+}
+
 static int get_args(uint32_t kernel, struct smq_invoke_ctx *ctx,
 			remote_arg_t *upra)
 {
@@ -905,8 +953,14 @@ static int get_args(uint32_t kernel, struct smq_invoke_ctx *ctx,
 		rlen -= mlen;
 	}
 
-	for (i = 0; i < inbufs; ++i) {
-		if (rpra[i].buf.len)
+	if (!kernel) {
+		VERIFY(err, 0 == clear_user_outbufs(ctx));
+		if (err)
+			goto bail;
+	}
+	for (oix = 0; oix < inbufs + outbufs; ++oix) {
+		int i = ctx->overps[oix]->raix;
+		if (rpra[i].buf.len && ctx->overps[oix]->mstart)
 			dmac_flush_range(rpra[i].buf.pv,
 				  (char *)rpra[i].buf.pv + rpra[i].buf.len);
 	}
@@ -989,8 +1043,11 @@ static void inv_args_pre(uint32_t sc, remote_arg_t *rpra)
 	}
 }
 
-static void inv_args(uint32_t sc, remote_arg_t *rpra, int used)
+static void inv_args(struct smq_invoke_ctx *ctx)
 {
+	uint32_t sc = ctx->sc;
+	remote_arg_t *rpra = ctx->rpra;
+	int used = ctx->obuf.used;
 	int i, inbufs, outbufs;
 	int inv = 0;
 
@@ -1015,6 +1072,10 @@ static int fastrpc_invoke_send(struct fastrpc_apps *me,
 {
 	struct smq_msg msg;
 	int err = 0, len;
+
+	VERIFY(err, 0 != me->channel[ctx->fdata->cid].chan);
+	if (err)
+		goto bail;
 	msg.pid = current->tgid;
 	msg.tid = current->pid;
 	if (kernel)
@@ -1028,6 +1089,7 @@ static int fastrpc_invoke_send(struct fastrpc_apps *me,
 	len = smd_write(me->channel[ctx->fdata->cid].chan, &msg, sizeof(msg));
 	spin_unlock(&me->wrlock);
 	VERIFY(err, len == sizeof(msg));
+ bail:
 	return err;
 }
 
@@ -1254,13 +1316,13 @@ static int fastrpc_internal_invoke(struct fastrpc_apps *me, uint32_t mode,
 
 	inv_args_pre(ctx->sc, ctx->rpra);
 	if (FASTRPC_MODE_SERIAL == mode)
-		inv_args(ctx->sc, ctx->rpra, ctx->obuf.used);
+		inv_args(ctx);
 	VERIFY(err, 0 == fastrpc_invoke_send(me, kernel, invoke->handle,
 						ctx->sc, ctx, &ctx->obuf));
 	if (err)
 		goto bail;
 	if (FASTRPC_MODE_PARALLEL == mode)
-		inv_args(ctx->sc, ctx->rpra, ctx->obuf.used);
+		inv_args(ctx);
  wait:
 	if (kernel)
 		wait_for_completion(&ctx->work);
