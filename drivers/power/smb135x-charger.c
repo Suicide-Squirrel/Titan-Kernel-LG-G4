82706536b49cca595c02ca4a1a41b4d4399add98
AndroidGX <subut3x@gmail.com>
2016-02-19 16:56:21 +0100

    add USB fast charge (credits to flar2)
    Signed-off-by: AndroidGX <subut3x@gmail.com>

--- drivers/power/smb135x-charger.c
+++ drivers/power/smb135x-charger.c
@@ -28,6 +28,9 @@
 #include <linux/regulator/of_regulator.h>
 #include <linux/regulator/machine.h>
 #include <linux/pinctrl/consumer.h>
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastcharge.h>
+#endif
 
 #ifdef CONFIG_LGE_PM_FACTORY_PSEUDO_BATTERY
 #include <soc/qcom/lge/board_lge.h>
@@ -1267,7 +1270,12 @@ static int smb135x_set_usb_chg_current(struct smb135x_chg *chip,
 #ifdef CONFIG_LGE_PM_PARALLEL_CHARGING
 #else
 	if (current_ma == CURRENT_500_MA) {
-		rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, 0);
+#ifdef CONFIG_FORCE_FAST_CHARGE
+		if (force_fast_charge)
+			rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, USB_2_3_BIT);
+		else
+#endif
+			rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, 0);
 		rc |= smb135x_masked_write(chip, CMD_INPUT_LIMIT,
 				USB_100_500_AC_MASK, USB_500_VAL);
 		rc |= smb135x_path_suspend(chip, USB, CURRENT, false);
@@ -2218,7 +2226,12 @@ static void smb135x_external_power_changed(struct power_supply *psy)
 
 	if (chip->usb_psy_ma != current_limit) {
 		mutex_lock(&chip->current_change_lock);
-		chip->usb_psy_ma = current_limit;
+#ifdef CONFIG_FORCE_FAST_CHARGE
+		if (force_fast_charge)
+			chip->usb_psy_ma = 900;
+		else
+#endif
+			chip->usb_psy_ma = current_limit;
 		rc = smb135x_set_appropriate_current(chip, USB);
 		mutex_unlock(&chip->current_change_lock);
 		if (rc < 0)
