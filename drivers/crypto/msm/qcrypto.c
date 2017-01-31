62cb5870f223bc2ca01f2e17aaa1b6623e9e3866
Zhen Kong <zkong@codeaurora.org>
2017-01-31 12:07:10 -0800

    crypto: msm: check length before copying to buf in _debug_stats_read
    Make sure that `len` is not larger than `count` before copying data
    to userspace `buf` in _debug_stats_read().
    
    Change-Id: Iafb7cfa3828653f8c28183c812797c3d9a183da1
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>

--- drivers/crypto/msm/qcrypto.c
+++ drivers/crypto/msm/qcrypto.c
@@ -1,6 +1,6 @@
 /* Qualcomm Crypto driver
  *
- * Copyright (c) 2010-2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2010-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
