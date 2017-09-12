8f8ac23454526ba0ad72731bd164b1449f3b26ca
kessaras <theodoroupaulos@gmail.com>
2017-09-12 10:25:50 +0300

    FIOPS: Tuned

--- block/fiops-iosched.c
+++ block/fiops-iosched.c
@@ -16,10 +16,10 @@
 #define VIOS_SCALE_SHIFT 10
 #define VIOS_SCALE (1 << VIOS_SCALE_SHIFT)
 
-#define VIOS_READ_SCALE (1)
-#define VIOS_WRITE_SCALE (1)
-#define VIOS_SYNC_SCALE (2)
-#define VIOS_ASYNC_SCALE (5)
+#define VIOS_READ_SCALE (8)
+#define VIOS_WRITE_SCALE (12)
+#define VIOS_SYNC_SCALE (8)
+#define VIOS_ASYNC_SCALE (10)
 
 #define VIOS_PRIO_SCALE (5)
 
