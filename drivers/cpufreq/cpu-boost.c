2b5a9e3507706b59d598b3096696783c621b0869
kessaras <theodoroupaulos@gmail.com>
2017-09-15 16:58:07 +0300

    cpu_boost: if minimum frequency is >= boost frequency , don't rise frequency

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -251,6 +251,9 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 		if (cpu == 4 && min > 0 && cnt_nr_running == 0)
                         break;
 
+		if (policy->cur >= ib_min)
+			break;
+
 		pr_debug("CPU%u policy min before boost: %u kHz\n",
 			 cpu, policy->min);
 		pr_debug("CPU%u boost min: %u kHz\n", cpu, min);
