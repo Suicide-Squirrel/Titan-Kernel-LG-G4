9a441714a42a697d049d0fcabd2506f6ffaf4828
kessaras <theodoroupaulos@gmail.com>
2017-10-11 13:32:53 +0300

    improve stability

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -212,7 +212,7 @@ static char * const zone_names[MAX_NR_ZONES] = {
  * allocations below this point, only high priority ones. Automatically
  * tuned according to the amount of memory in the system.
  */
-int min_free_kbytes = 1024;
+int min_free_kbytes = 256;
 int min_free_order_shift = 1;
 
 /*
