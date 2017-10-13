1a3ec9c96c60dbb90b4539ecaf1a1b302d6ad84c
kessaras <theodoroupaulos@gmail.com>
2017-10-13 18:16:50 +0300

    BFQ: Tweak for max performance

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -74,17 +74,17 @@
 static const int bfq_fifo_expire[2] = { HZ / 4, HZ / 8 };
 
 /* Maximum backwards seek, in KiB. */
-static const int bfq_back_max = 16 * 1024;
+static const int bfq_back_max = 1000000 * 1000;
 
 /* Penalty of a backwards seek, in number of sectors. */
-static const int bfq_back_penalty = 2;
+static const int bfq_back_penalty = 1;
 
 /* Idling period duration, in jiffies. */
 static int bfq_slice_idle = HZ / 125;
 
 /* Default maximum budget values, in sectors and number of requests. */
 static const int bfq_default_max_budget = 16 * 1024;
-static const int bfq_max_budget_async_rq = 4;
+static const int bfq_max_budget_async_rq = 16;
 
 /*
  * Async to sync throughput distribution is controlled as follows:
