7d1a124ba00ece664375820ae640b420614c21ca
Todd Kjos <tkjos@google.com>
2017-12-06 22:20:13 +0100

    ANDROID: binder: fix OOB write in __binder_update_page_range
    commit e12f1f1c65a9 ("binder: always allocate/map first
    BINDER_MIN_ALLOC pages") introduced a vulnerability where
    the alloc->pages array can be accessed beyond the allocated
    elements. For this to occur, the passed-in VMA range must
    be < BINDER_MIN_ALLOC pages (which cannot happen for normal
    use cases). BINDER_MIN_ALLOC is set to 6 pages, so mmap'ing
    1 page results in OOB accesses.
    
    The fix is to return an error if binder_mmap is called with
    a VMA size less than BINDER_MIN_ALLOC.
    
    Bug: 64216036
    Test: verified fixed with submitter's test program
    Change-Id: I4b6dbf3f466fea46e587d6a33f724ee4fe7f1368
    Signed-off-by: Todd Kjos <tkjos@google.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3493,6 +3493,11 @@ static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
 		}
 	}
 #endif
+	if (vma->vm_end - vma->vm_start < BINDER_MIN_ALLOC) {
+		ret = -EINVAL;
+		failure_string = "VMA size < BINDER_MIN_ALLOC";
+		goto err_vma_too_small;
+	}
 	proc->pages = kzalloc(sizeof(proc->pages[0]) * ((vma->vm_end - vma->vm_start) / PAGE_SIZE), GFP_KERNEL);
 	if (proc->pages == NULL) {
 		ret = -ENOMEM;
@@ -3541,6 +3546,7 @@ err_alloc_buf_struct_failed:
 	kfree(proc->pages);
 	proc->pages = NULL;
 err_alloc_pages_failed:
+err_vma_too_small:
 	mutex_lock(&proc->context->binder_mmap_lock);
 	vfree(proc->buffer);
 	proc->buffer = NULL;
