c42f33582a5ad00fc48d8d705ef353b4fb85a3d5
steadfasterX <steadfasterX>
2018-03-08 19:50:08 +0100

    this always return -2 / no such file..
    Change-Id: I8dd2387d4d92e75558670b419b9b78c183fda31c

--- drivers/soc/qcom/usu_detect.c
+++ drivers/soc/qcom/usu_detect.c
@@ -31,10 +31,34 @@ static size_t usu_offset __initdata=3145722;
 static char usu_model[200] __initdata="undef";
 struct file *f;
 
-int __init file_read(struct file *file, size_t offset, unsigned char *data, unsigned int size) 
+struct __init file *file_open(const char *path, int flags, int rights) 
 {
-    printk("UsU: (%s) reading offset %lu\n", __func__, offset);
-/**
+    pr_info("UsU (%s): reading file %s\n", __func__, path);
+
+    struct file *filp = NULL;
+    mm_segment_t oldfs;
+    int err = 0;
+
+    oldfs = get_fs();
+    set_fs(KERNEL_DS);
+
+    filp = filp_open(path, flags, rights);
+    set_fs(oldfs);
+    if (IS_ERR(filp)) {
+    	pr_info("UsU (%s): error %d while opening file\n", __func__, PTR_ERR(filp));
+        err = PTR_ERR(filp);
+        return NULL;
+    }
+
+    pr_info("UsU (%s): end\n", __func__);
+    return filp;
+    //return 0;
+}
+
+int __init file_read(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size) 
+{
+    pr_info("UsU (%s): reading offset %llu\n", __func__, offset);
+
     mm_segment_t oldfs;
     int ret;
 
@@ -44,26 +68,31 @@ int __init file_read(struct file *file, size_t offset, unsigned char *data, unsi
     ret = vfs_read(file, data, size, &offset);
 
     set_fs(oldfs);
+    pr_info("UsU (%s): end\n", __func__);
     return ret;
-**/
-    return 0;
-}  
+}
 
 void __init file_close(struct file *file) 
 {
-    printk("UsU (%s): closing disk\n", __func__);
+    pr_info("UsU (%s): closing disk\n", __func__);
     //filp_close(file, NULL);
+    pr_info("UsU (%s): end\n", __func__);
 }
 
 static int __init get_usu_model(void)
 {
     pr_info("UsU (%s): usu_partition: %s, usu_offset: %lu, usu_model: %s\n", __func__, usu_partition, usu_offset, usu_model);
-    f = filp_open(usu_partition,O_RDONLY,0644);
-    file_read(f, usu_offset, usu_model, 7);
+    f = file_open(usu_partition, O_RDONLY, 0);
+    //file_read(f, usu_offset, usu_model, 7);
     file_close(f);
-    printk("UsU (%s): model: %s\n", __func__, usu_model);
+    pr_info("UsU (%s): model: %s\n", __func__, usu_model);
+    pr_info("UsU (%s): end\n", __func__);
     return 0;
 }
 
 /** command-line is loaded at core_initcall() **/
-subsys_initcall(get_usu_model);
+//subsys_initcall(get_usu_model);
+
+// https://stackoverflow.com/questions/18605653/module-init-vs-core-initcall-vs-early-initcall#18606561
+late_initcall(get_usu_model);
+
