1c8fc0c5b0b8a7e8e370661c987294e94a38842d
WiZaRd981 <pobricharun@gmail.com>
2019-05-17 15:41:23 +0000

    cpufreq: Add Relaxed governor
    (cherry picked from commit b6afea365b44cb447c16a4c99c05b21897fa0fab)

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -191,7 +191,12 @@ struct cs_dbs_tuners {
 	unsigned int sampling_down_factor;
 	unsigned int up_threshold;
 	unsigned int down_threshold;
+        unsigned int down_threshold_suspended;
 	unsigned int freq_step;
+        unsigned int sleep_depth;
+        unsigned int boost_enabled;
+        unsigned int boost_count;
+        unsigned int boost_ceiling;
 };
 
 struct ex_dbs_tuners {
