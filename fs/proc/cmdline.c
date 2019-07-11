8b44c7e1470edd7c8e0437478d8cec510fb34cbe
Sultanxda <sultanxda@gmail.com>
2019-07-11 10:41:34 +0200

    proc: pass safetynet
    removes SafetyNet flags, bootlock status and set androidboot.verifiedbootstate=green
    
    This commit is based on:
    https://github.com/steadfasterX/kernel_lge_llamasweet/commit/ae6f3c6fb5d905c60697671b3d4629b51b825da0
    https://github.com/pascua28/android_kernel_msm8917_ATU/commit/a7c9b4c732ccebec942af0b33800590e06c1c25d
    
    Adapted to LGE msm8992 pie (steadfasterX) to ensure offline charging does not break.
    
    Co-authored-by: Samuel Pascua <pascua.samuel.14@gmail.com>
    Co-authored-by: steadfasterX <steadfasterX@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -8,39 +8,12 @@
 #include <soc/qcom/lge/board_lge.h>
 #endif
 
+static char proc_cmdline[COMMAND_LINE_SIZE];
 static char updated_command_line[COMMAND_LINE_SIZE];
-
-static void proc_cmdline_set(char *name, char *value)
-{
-	char *flag_pos, *flag_after;
-	char *flag_pos_str = kmalloc(sizeof(char), COMMAND_LINE_SIZE);
-
-	scnprintf(flag_pos_str, COMMAND_LINE_SIZE, "%s=", name);
-
-	flag_pos = strstr(updated_command_line, flag_pos_str);
-	if (flag_pos) {
-		flag_after = strchr(flag_pos, ' ');
-		if (!flag_after)
-			flag_after = "";
-
-		scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%.*s%s=%s%s",
-				(int)(flag_pos - updated_command_line),
-				updated_command_line, name, value, flag_after);
-	} else {
-		// flag was found, insert it
-		scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%s %s=%s", updated_command_line, name, value);
-	}
-}
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-#ifdef CONFIG_MACH_LGE
-	if (lge_get_boot_mode() == LGE_BOOT_MODE_CHARGERLOGO) {
-		proc_cmdline_set("androidboot.mode", "charger");
-	}
-#endif
-
-	seq_printf(m, "%s\n", updated_command_line);
+	seq_printf(m, "%s\n", proc_cmdline);
 	return 0;
 }
 
@@ -56,11 +29,69 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+static void remove_flag(char *cmd, const char *flag)
+{
+        char *start_addr, *end_addr;
+
+        /* Ensure all instances of a flag are removed */
+        while ((start_addr = strstr(cmd, flag))) {
+                end_addr = strchr(start_addr, ' ');
+                if (end_addr)
+                        memmove(start_addr, end_addr + 1, strlen(end_addr));
+                else
+                        *(start_addr - 1) = '\0';
+        }
+}
+
+static void remove_safetynet_flags(char *cmd)
+{
+        remove_flag(cmd, "androidboot.enable_dm_verity=");
+        remove_flag(cmd, "androidboot.secboot=");
+        remove_flag(cmd, "androidboot.veritymode=");
+        remove_flag(cmd, "bootlock=");
+}
+
 static int __init proc_cmdline_init(void)
 {
-	// copy it only once
-	strcpy(updated_command_line, saved_command_line);
+        char *a1, *a2;
 
+#ifdef CONFIG_MACH_LGE
+	char *chg;
+        if (lge_get_boot_mode() == LGE_BOOT_MODE_CHARGERLOGO) {
+	    chg = strstr(saved_command_line, "androidboot.mode=");
+	    if (chg) {
+		chg = strchr(chg, '=');
+		a2 = strchr(chg, ' ');
+		if (!a2) /* last argument on the cmdline */
+		    a2 = "";
+		scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%.*scharger%s",
+                          (int)(chg - saved_command_line + 1),
+                          saved_command_line, a2);
+	    } else {
+		strcpy(updated_command_line, saved_command_line);
+	    }
+	} else {
+#endif
+	    strcpy(updated_command_line, saved_command_line);
+#ifdef CONFIG_MACH_LGE
+	}
+#endif
+	/* SafetyNet bypass: show androidboot.verifiedbootstate=green */
+	a1 = strstr(updated_command_line, "androidboot.verifiedbootstate=");
+	if (a1) {
+            a1 = strchr(a1, '=');
+            a2 = strchr(a1, ' ');
+            if (!a2) /* last argument on the cmdline */
+                    a2 = "";
+
+            scnprintf(proc_cmdline, COMMAND_LINE_SIZE, "%.*sgreen%s",
+                          (int)(a1 - updated_command_line + 1),
+                          updated_command_line, a2);
+	} else {
+            strncpy(proc_cmdline, updated_command_line, COMMAND_LINE_SIZE);
+	}
+
+	remove_safetynet_flags(proc_cmdline);
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
