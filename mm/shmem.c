a91373cb9f4ae564da6e9ca59c9425e0e866a6ca
pavlaras <theodoroupaulos@gmail.com>
2020-07-29 08:37:23 +0300

    Revert "mm: shmem: Reschedule by unlocking and relocking RCU because of missing API"
    This reverts commit 4f83a5d9214c2c7e271dcc492a17b8fa261ffec7.

--- mm/shmem.c
+++ mm/shmem.c
@@ -1923,8 +1923,7 @@ restart:
 		}
 
 		if (need_resched()) {
-			rcu_read_unlock();
-			rcu_read_lock();
+			cond_resched_rcu();
 			start = iter.index + 1;
 			goto restart;
 		}
@@ -1994,8 +1993,7 @@ restart:
 			spin_unlock_irq(&mapping->tree_lock);
 continue_resched:
 			if (need_resched()) {
-				rcu_read_unlock();
-				rcu_read_lock();
+				cond_resched_rcu();
 				start = iter.index + 1;
 				goto restart;
 			}
