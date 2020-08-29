franciscofranco/one_plus_2

power: smbcharger: queue work on system_power_efficient_wq
There doesn't seem to be any real dependency of scheduling these on
the cpu which scheduled them, so moving every *_delayed to the
power efficient wq save potential needlessly idle cpu wake ups
leaving the scheduler to decide the most appropriate cpus to wake up.

Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

drivers/power/qpnp-smbcharger.c
@@ -2430,7 +2430,7 @@ static void smbchg_parallel_usb_check_ok(struct smbchg_chip *chip)
 	mutex_lock(&chip->parallel.lock);
 	if (smbchg_is_parallel_usb_ok(chip)) {
 		smbchg_stay_awake(chip, PM_PARALLEL_CHECK);
-		schedule_delayed_work(
+		queue_delayed_work(system_power_efficient_wq,
 			&chip->parallel_en_work,
 			msecs_to_jiffies(PARALLEL_CHARGER_EN_DELAY_MS));
 	} else if (chip->parallel.current_max_ma != 0) {
@@ -3189,7 +3189,8 @@ static void smbchg_vfloat_adjust_check(struct smbchg_chip *chip)
 
 	smbchg_stay_awake(chip, PM_REASON_VFLOAT_ADJUST);
 	pr_smb(PR_STATUS, "Starting vfloat adjustments\n");
-	schedule_delayed_work(&chip->vfloat_adjust_work, 0);
+	queue_delayed_work(system_power_efficient_wq,
+		&chip->vfloat_adjust_work, 0);
 }
 
 #define FV_STS_REG			0xC
@@ -4153,8 +4154,9 @@ stop:
 	return;
 
 reschedule:
-	schedule_delayed_work(&chip->vfloat_adjust_work,
-			msecs_to_jiffies(VFLOAT_RESAMPLE_DELAY_MS));
+	queue_delayed_work(system_power_efficient_wq,
+		&chip->vfloat_adjust_work,
+		msecs_to_jiffies(VFLOAT_RESAMPLE_DELAY_MS));
 	return;
 }
 
@@ -4326,8 +4328,9 @@ static void handle_usb_insertion(struct smbchg_chip *chip)
 	}
 
 	if (usb_supply_type == POWER_SUPPLY_TYPE_USB_DCP)
-		schedule_delayed_work(&chip->hvdcp_det_work,
-					msecs_to_jiffies(HVDCP_NOTIFY_MS));
+		queue_delayed_work(system_power_efficient_wq,
+			&chip->hvdcp_det_work,
+			msecs_to_jiffies(HVDCP_NOTIFY_MS));
 
 	mutex_lock(&chip->current_change_lock);
 	if (usb_supply_type == POWER_SUPPLY_TYPE_USB)
@@ -6834,7 +6837,8 @@ static int qpnp_start_charging(struct smbchg_chip *chip)
 						smbchg_rerun_aicl(chip);
 					} else {
 						if(chip->aicl_current == 0) {
-							schedule_delayed_work(&chip->soft_aicl_work,
+							queue_delayed_work(system_power_efficient_wq,
+								&chip->soft_aicl_work,
 								msecs_to_jiffies(SOFT_AICL_DELAY_MS));
 						} else {
 							if (chip->aicl_current >= chip->temp_cool_current	) {
@@ -6864,7 +6868,8 @@ static int qpnp_start_charging(struct smbchg_chip *chip)
 						smbchg_set_usb_current_max(chip, ret.intval / 1000);
 					} else {
 						if(chip->aicl_current == 0) {
-							schedule_delayed_work(&chip->soft_aicl_work,
+								queue_delayed_work(system_power_efficient_wq,
+								&chip->soft_aicl_work,
 								msecs_to_jiffies(SOFT_AICL_DELAY_MS));
 						} else {
 							if (chip->aicl_current >= chip->temp_littel_cool_current) {
@@ -6893,7 +6898,8 @@ static int qpnp_start_charging(struct smbchg_chip *chip)
 							smbchg_set_usb_current_max(chip, ret.intval / 1000);
 						} else {
 							if(chip->aicl_current == 0) {
-								schedule_delayed_work(&chip->soft_aicl_work,
+								queue_delayed_work(system_power_efficient_wq,
+									&chip->soft_aicl_work,
 									msecs_to_jiffies(SOFT_AICL_DELAY_MS));
 							} else {
 								if (chip->aicl_current >= chip->temp_normal_current) {
@@ -6921,7 +6927,8 @@ static int qpnp_start_charging(struct smbchg_chip *chip)
 						smbchg_set_usb_current_max(chip, ret.intval / 1000);
 					} else {
 						if(chip->aicl_current == 0) {
-							schedule_delayed_work(&chip->soft_aicl_work,
+							queue_delayed_work(system_power_efficient_wq,
+								&chip->soft_aicl_work,
 								msecs_to_jiffies(SOFT_AICL_DELAY_MS));
 						} else {
 							if (chip->aicl_current >= chip->temp_warm_current) {
@@ -7155,7 +7162,8 @@ static int handle_batt_temp_little_cold(struct smbchg_chip *chip)
 				chip->usb_target_current_ma=500;/* yangfangbiao@oneplus.cn,20150710  Add for usb thermal current limit */
 			} else {
 				if(chip->aicl_current == 0) {
-					schedule_delayed_work(&chip->soft_aicl_work,
+					queue_delayed_work(system_power_efficient_wq,
+						&chip->soft_aicl_work,
 						msecs_to_jiffies(SOFT_AICL_DELAY_MS));
 
 					if((chip->oem_lcd_is_on==true)&&(chip->aicl_current > chip->lcd_on_iusb))
@@ -7226,7 +7234,8 @@ static int handle_batt_temp_cool(struct smbchg_chip *chip)
 					smbchg_rerun_aicl(chip);
 				} else {
 					if(chip->aicl_current == 0) {
-						schedule_delayed_work(&chip->soft_aicl_work,
+						queue_delayed_work(system_power_efficient_wq,
+							&chip->soft_aicl_work,
 							msecs_to_jiffies(SOFT_AICL_DELAY_MS));
 						if((chip->oem_lcd_is_on==true)&&(chip->aicl_current > chip->lcd_on_iusb))
 								{
@@ -7305,7 +7314,8 @@ static int handle_batt_temp_little_cool(struct smbchg_chip *chip)
 						smbchg_rerun_aicl(chip);
 					} else {
 						if(chip->aicl_current == 0) {
-							schedule_delayed_work(&chip->soft_aicl_work,
+							queue_delayed_work(system_power_efficient_wq,
+								&chip->soft_aicl_work,
 								msecs_to_jiffies(SOFT_AICL_DELAY_MS));
 							if((chip->oem_lcd_is_on==true)&&(chip->aicl_current > chip->lcd_on_iusb))
 								{
@@ -7389,7 +7399,8 @@ static int handle_batt_temp_pre_normal(struct smbchg_chip *chip)
 			smbchg_rerun_aicl(chip);
 		} else {
 			if(chip->aicl_current == 0) {
-				schedule_delayed_work(&chip->soft_aicl_work,
+				queue_delayed_work(system_power_efficient_wq,
+					&chip->soft_aicl_work,
 					msecs_to_jiffies(SOFT_AICL_DELAY_MS));
 				if((chip->oem_lcd_is_on==true)&&(chip->aicl_current > chip->lcd_on_iusb)) {
 					smbchg_set_usb_current_max(chip, calc_thermal_limited_current(chip, chip->lcd_on_iusb));
@@ -7461,7 +7472,8 @@ static int handle_batt_temp_normal(struct smbchg_chip *chip)
 						smbchg_rerun_aicl(chip);
 					} else {
 						if(chip->aicl_current == 0) {
-							schedule_delayed_work(&chip->soft_aicl_work,
+							queue_delayed_work(system_power_efficient_wq,
+								&chip->soft_aicl_work,
 								msecs_to_jiffies(SOFT_AICL_DELAY_MS));
 							if((chip->oem_lcd_is_on==true)&&(chip->aicl_current > chip->lcd_on_iusb))
 								{
@@ -7540,7 +7552,8 @@ static int handle_batt_temp_warm(struct smbchg_chip *chip)
 						smbchg_rerun_aicl(chip);
 					} else {
 						if(chip->aicl_current == 0) {
-							schedule_delayed_work(&chip->soft_aicl_work,
+							queue_delayed_work(system_power_efficient_wq,
+								&chip->soft_aicl_work,
 								msecs_to_jiffies(SOFT_AICL_DELAY_MS));
 							if((chip->oem_lcd_is_on==true)&&(chip->aicl_current > chip->lcd_on_iusb))
 								{
@@ -7878,8 +7891,9 @@ static void update_heartbeat(struct work_struct *work)
 	power_supply_changed(&chip->batt_psy);
 
 	/*update time 6s*/
-	schedule_delayed_work(&chip->update_heartbeat_work,
-			      round_jiffies_relative(msecs_to_jiffies
+	queue_delayed_work(system_power_efficient_wq,
+		&chip->update_heartbeat_work,
+		round_jiffies_relative(msecs_to_jiffies
 						     (BATT_HEARTBEAT_INTERVAL)));
 	//qpnp_dump_info(chip);
 
@@ -8211,7 +8225,8 @@ static int smbchg_probe(struct spmi_device *spmi)
 
 	INIT_DELAYED_WORK(&chip->update_heartbeat_work,
 							update_heartbeat);
-	schedule_delayed_work(&chip->update_heartbeat_work,
+		queue_delayed_work(system_power_efficient_wq,
+					&chip->update_heartbeat_work,
 				  round_jiffies_relative(msecs_to_jiffies
 						(BATT_HEARTBEAT_INTERVAL)));
 

Suicide-Squirrel/Titan-Kernel-LG-G4

Change-Id: Ia1499f86401f868e3cbf1a80d5d664290e5368ee

drivers/power/qpnp-smbcharger.c
@@ -2174,7 +2174,7 @@ static void smbchg_parallel_usb_enable(struct smbchg_chip *chip)
 	if (!smbchg_is_aicl_complete(chip))
 	{
 		pr_smb(PR_LGE, "aicl is not complete. retry later.\n");
-		schedule_delayed_work(
+		queue_delayed_work(system_power_efficient_wq,
 			&chip->parallel_en_work,
 			msecs_to_jiffies(RETRY_TIME_MS));
 		return;
@@ -2335,7 +2335,7 @@ static void smbchg_parallel_usb_check_ok(struct smbchg_chip *chip)
 	mutex_lock(&chip->parallel.lock);
 	if (smbchg_is_parallel_usb_ok(chip)) {
 		smbchg_stay_awake(chip, PM_PARALLEL_CHECK);
-		schedule_delayed_work(
+		queue_delayed_work(system_power_efficient_wq,
 			&chip->parallel_en_work,
 			msecs_to_jiffies(PARALLEL_CHARGER_EN_DELAY_MS));
 	} else if (chip->parallel.current_max_ma != 0) {
@@ -3084,7 +3084,8 @@ static int smbchg_battery_set_property(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_ENABLE_EVP_CHG:
 		chip->is_evp_ta = val->intval;
 		pr_smb(PR_LGE, "is_evp_ta = %d\n", chip->is_evp_ta);
-		schedule_delayed_work(&chip->enable_evp_chg_work, 0);
+		queue_delayed_work(system_power_efficient_wq,
+			&chip->enable_evp_chg_work, 0);
 		break;
 #endif
 #ifdef CONFIG_LGE_PM_QC20_SCENARIO
@@ -3312,7 +3313,8 @@ static void smbchg_vfloat_adjust_check(struct smbchg_chip *chip)
 
 	smbchg_stay_awake(chip, PM_REASON_VFLOAT_ADJUST);
 	pr_smb(PR_STATUS, "Starting vfloat adjustments\n");
-	schedule_delayed_work(&chip->vfloat_adjust_work, 0);
+	queue_delayed_work(system_power_efficient_wq,
+		&chip->vfloat_adjust_work, 0);
 }
 
 #define FV_STS_REG			0xC
@@ -4261,8 +4263,9 @@ stop:
 	return;
 
 reschedule:
-	schedule_delayed_work(&chip->vfloat_adjust_work,
-			msecs_to_jiffies(VFLOAT_RESAMPLE_DELAY_MS));
+	queue_delayed_work(system_power_efficient_wq,
+		&chip->vfloat_adjust_work,
+		msecs_to_jiffies(VFLOAT_RESAMPLE_DELAY_MS));
 	return;
 }
 
@@ -4738,7 +4741,8 @@ static void handle_usb_removal(struct smbchg_chip *chip)
 		wake_unlock(&chip->hvdcp_lock);
 #endif
 #ifdef CONFIG_LGE_PM_BMD
-	schedule_delayed_work(&chip->update_bmd_work, 0);
+	queue_delayed_work(system_power_efficient_wq,
+	&chip->update_bmd_work, 0);
 #endif
 
 }
@@ -4843,7 +4847,8 @@ static void handle_usb_insertion(struct smbchg_chip *chip)
 	}
 
 	if (usb_supply_type == POWER_SUPPLY_TYPE_USB_DCP)
-		schedule_delayed_work(&chip->hvdcp_det_work,
+		queue_delayed_work(system_power_efficient_wq,
+					&chip->hvdcp_det_work,
 					msecs_to_jiffies(HVDCP_NOTIFY_MS));
 	if (parallel_psy) {
 		rc = power_supply_set_present(parallel_psy, true);
@@ -4858,7 +4863,8 @@ static void handle_usb_insertion(struct smbchg_chip *chip)
 		chip->enable_aicl_wake = true;
 	}
 #ifdef CONFIG_LGE_PM_BMD
-	schedule_delayed_work(&chip->update_bmd_work, 0);
+	queue_delayed_work(system_power_efficient_wq,
+	&chip->update_bmd_work, 0);
 #endif
 }
 
@@ -5362,7 +5368,8 @@ static irqreturn_t aicl_done_handler(int irq, void *_chip)
 #endif
 		if (aicl_rerun && (aicl_status == AICL_RERUN)) {
 			aicl_status = AICL_ENABLE;
-			schedule_delayed_work(&chip->aicl_work,
+			queue_delayed_work(system_power_efficient_wq,
+						&chip->aicl_work,
 						msecs_to_jiffies(AICL_RERUN_TIME));
 				}
 	}